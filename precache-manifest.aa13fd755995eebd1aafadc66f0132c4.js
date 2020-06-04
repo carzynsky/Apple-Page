9fdf035bbeb132507e29d7200179f9ef86a8b4c9
Arek <Arek@DESKTOP-U4VRK31>
2020-06-04 21:47:20 +0200

    Updates

---
+++ precache-manifest.aa13fd755995eebd1aafadc66f0132c4.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "31c486b718d2c5d3b8eef833854dc8b6",
+    "url": "/Apple-Page/index.html"
+  },
+  {
+    "revision": "15d0a2288ecf3db49ded",
+    "url": "/Apple-Page/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d3ab73d51692b3f5976e",
+    "url": "/Apple-Page/static/css/main.f3fa142d.chunk.css"
+  },
+  {
+    "revision": "15d0a2288ecf3db49ded",
+    "url": "/Apple-Page/static/js/2.ebbee8e3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Apple-Page/static/js/2.ebbee8e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d3ab73d51692b3f5976e",
+    "url": "/Apple-Page/static/js/main.b641353d.chunk.js"
+  },
+  {
+    "revision": "6ca8dd5825d5e663f7da",
+    "url": "/Apple-Page/static/js/runtime-main.89a8c668.js"
+  },
+  {
+    "revision": "3bb585ed377be514bf060385573e292c",
+    "url": "/Apple-Page/static/media/dark.3bb585ed.png"
+  },
+  {
+    "revision": "849f06a43741a3f8d3f1c6fc19a995f0",
+    "url": "/Apple-Page/static/media/iPad.849f06a4.png"
+  },
+  {
+    "revision": "faa2f043f247b4dce2c433737d24ede4",
+    "url": "/Apple-Page/static/media/iPhoneSE.faa2f043.png"
+  },
+  {
+    "revision": "e64356057469a6ca7a469a5114be61ac",
+    "url": "/Apple-Page/static/media/iphone11.e6435605.png"
+  },
+  {
+    "revision": "50dea95df051d05fcf0f0e5fd040ec98",
+    "url": "/Apple-Page/static/media/light.50dea95d.png"
+  },
+  {
+    "revision": "e4834d59b3b73fa6501bf05c1642afaa",
+    "url": "/Apple-Page/static/media/macPro13.e4834d59.png"
+  }
+]);
