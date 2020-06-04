104357b7fa19589865faa2e490559b23ed4a27de
Arek <Arek@DESKTOP-U4VRK31>
2020-06-04 21:56:19 +0200

    Fix paths in Nav

--- src/NavBar.js
+++ src/NavBar.js
@@ -11,10 +11,10 @@ const NavBar = () => {
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="myNav">
-                            <NavLink exact activeStyle={{ color: '#ffffff' }} className="Nav-Link" to= '/apple'><img src={logo} className="pic" alt="logo"/></NavLink>
-                            <NavLink activeStyle={{ color: '#ffffff' }} className="Nav-Link" to="/apple/mac">Mac</NavLink>
-                            <NavLink activeStyle={{ color: '#ffffff' }} className="Nav-Link" to="/apple/iPad">iPad</NavLink>
-                            <NavLink activeStyle={{ color: '#ffffff' }} className="Nav-Link" to="/apple/iPhone">iPhone</NavLink>
+                            <NavLink exact activeStyle={{ color: '#ffffff' }} className="Nav-Link" to= '/Apple-Page/'><img src={logo} className="pic" alt="logo"/></NavLink>
+                            <NavLink activeStyle={{ color: '#ffffff' }} className="Nav-Link" to="/Apple-Page/mac">Mac</NavLink>
+                            <NavLink activeStyle={{ color: '#ffffff' }} className="Nav-Link" to="/Apple-Page/iPad">iPad</NavLink>
+                            <NavLink activeStyle={{ color: '#ffffff' }} className="Nav-Link" to="/Apple-Page/iPhone">iPhone</NavLink>
                         </Nav>
                      </Navbar.Collapse>
                 </Navbar>
